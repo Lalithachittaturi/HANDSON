e5487e44bf74ba1299244b71c9507ccd028ad81d
Lalithachittaturi <51713222+Lalithachittaturi@users.noreply.github.com>
2020-10-06 22:54:21 +0530

    Add files via upload

---
+++ HelloWorldServlet2.java
@@ -0,0 +1,26 @@
+package gui;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/HelloWorldServlet2")
+public class HelloWorldServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+      public HelloWorldServlet2() {
+        super();
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		out.println("<html>");
+		out.println("<h1 style=\"text-align: center;\">Welcome to Hall Paradise </h1");
+		out.println("<nav><a href=\"Exhibition.html\">Exhibition</a></nav>");
+		out.println("<nav><a href=\"Stageshow.html\">Stage show</a></nav>");	
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
